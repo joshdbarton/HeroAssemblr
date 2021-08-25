a73fc77193391379b5b13454a2c2905a14627cef
Josh Barton <joshdbarton@gmail.com>
2021-08-25 12:55:00 -0500

    typo and removing args example

--- Program.cs
+++ Program.cs
@@ -7,17 +7,12 @@ namespace HeroAssembler
     {
         static void Main(string[] args)
         {
-            // args is an array of strings passed from the console when the program starts
-            foreach (string arg in args)
-            {
-                Console.Write($" {arg}");
-            }
 
             // passing parameter to constructor
             Alliance avengers = new Alliance("Avengers");
             Console.WriteLine($"Named Alliance: {avengers.Name}");
 
-            //call a methods on the class instance
+            //call a method on the class instance
             avengers.Assemble();
 
             // using the object initializer 
